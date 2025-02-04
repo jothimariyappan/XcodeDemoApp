68db4c522d59f2318c9e60ae19adee3b9fc27201
jothiredblox <jothimariyappan.s@redblox.io>
2025-02-04 10:08:58 +0530

    Initial Commit

---
+++ XcodeDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeDemoApp
+//
+//  Created by jo on 04/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by jo on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
