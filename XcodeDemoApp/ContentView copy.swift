8051dc09abfe1c78ae07712f5d5d59b12faa2e52
jothimariyappan <jothimariyappan6@gmail.com>
2025-02-08 13:50:28 +0530

    new

---
+++ XcodeDemoApp/ContentView copy.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  XcodeDemoApp
+//
+//  Created by jo on 04/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Text("this from staging branch")
+            Text("for fetch test code written in staging branch")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
